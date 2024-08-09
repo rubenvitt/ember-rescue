adc1e625bd641a8b9c8aa5767ec21e6ad5d89645
Ruben Vitt <dev@rubeen.dev>
2024-08-09 14:39:21 +0200

    ⚗️(einsatz-service): add tests for alarmstichwort changes
    - Added mocks for EinsatztagebuchService, EinheitenService, and AlarmstichwortService.
    - Introduced tests to handle changes in alarmstichwort.
    - Included jest setting for static system time to '2024-08-09T12:19:50.671Z'.
    - Expanded test coverage for getEinsatz method.
    
    Took 28 minutes

--- backend/src/einsatz/einsatz.service.spec.ts
+++ backend/src/einsatz/einsatz.service.spec.ts
@@ -1,20 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EinsatzService } from './einsatz.service';
 import { PrismaService } from '../database/prisma/prisma.service';
+import { EinsatztagebuchService } from '../einsatztagebuch/einsatztagebuch.service';
+import { EinheitenService } from '../einheiten/einheiten.service';
+import { AlarmstichwortService } from '../alarmstichwort/alarmstichwort.service';
 import { Prisma } from '@prisma/client';
+import { UpdateEinsatzDto } from '../types';
+
+jest.useFakeTimers().setSystemTime(new Date('2024-08-09T12:19:50.671Z'));
 
 describe('EinsatzService', () => {
   let service: EinsatzService;
   let prismaService: PrismaService;
+  let einsatztagebuchService: EinsatztagebuchService;
+  let einheitenService: EinheitenService;
+  let alarmstichwortService: AlarmstichwortService;
 
   beforeEach(async () => {
     const prismaMock = {
       einsatz: {
         findUnique: jest.fn().mockResolvedValue({
           id: 'einsatz1',
+          aufnehmendesRettungsmittelId: 'rettungsmittel1',
           einsatz_alarmstichwort: [
             {
               alarmstichwort: {
+                id: 'stichwort1',
                 bezeichnung: 'Test',
                 beschreibung: 'Test Beschreibung',
               },
@@ -41,15 +52,37 @@ describe('EinsatzService', () => {
       },
     };
 
+    const einsatztagebuchMock = {
+      createEinsatztagebuchEintrag: jest.fn().mockResolvedValue(null),
+    };
+
+    const einheitenMock = {
+      findEinheit: jest.fn().mockResolvedValue({ funkrufname: 'TestFunk' }),
+    };
+
+    const alarmstichwortMock = {
+      find: jest.fn().mockResolvedValue({ bezeichnung: 'New Test' }),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         EinsatzService,
         { provide: PrismaService, useValue: prismaMock },
+        { provide: EinsatztagebuchService, useValue: einsatztagebuchMock },
+        { provide: EinheitenService, useValue: einheitenMock },
+        { provide: AlarmstichwortService, useValue: alarmstichwortMock },
       ],
     }).compile();
 
     service = module.get<EinsatzService>(EinsatzService);
     prismaService = module.get<PrismaService>(PrismaService);
+    einsatztagebuchService = module.get<EinsatztagebuchService>(
+      EinsatztagebuchService,
+    );
+    einheitenService = module.get<EinheitenService>(EinheitenService);
+    alarmstichwortService = module.get<AlarmstichwortService>(
+      AlarmstichwortService,
+    );
   });
 
   it('should be defined', () => {
@@ -61,7 +94,9 @@ describe('EinsatzService', () => {
     const result = await service.getEinsatz(id);
     expect(result).toEqual({
       id,
+      aufnehmendesRettungsmittelId: 'rettungsmittel1',
       einsatz_alarmstichwort: {
+        id: 'stichwort1',
         bezeichnung: 'Test',
         beschreibung: 'Test Beschreibung',
       },
@@ -134,7 +169,62 @@ describe('EinsatzService', () => {
     expect(result.abgeschlossen.getTime()).toBeCloseTo(currentTime, -3);
     expect(prismaService.einsatz.update).toHaveBeenCalledWith({
       where: { id: einsatzId },
-      data: { abgeschlossen: expect.any(Date) },
+      data: { abgeschlossen: new Date() },
+    });
+  });
+
+  it('should change einsatz if alarmstichwort has changed', async () => {
+    const einsatzId = 'einsatz1';
+    const updateEinsatzDto: UpdateEinsatzDto = { alarmstichwort: 'newId' };
+
+    const result = await service.changeEinsatz(einsatzId, updateEinsatzDto);
+
+    expect(alarmstichwortService.find).toHaveBeenCalledWith('newId');
+    expect(einheitenService.findEinheit).toHaveBeenCalledWith({
+      id: 'rettungsmittel1',
+    });
+    expect(
+      einsatztagebuchService.createEinsatztagebuchEintrag,
+    ).toHaveBeenCalledWith({
+      einsatzId,
+      type: 'LAGE', // Adjust if 'LAGE' is from an enum or other source
+      content: 'Das Alarmstichwort wurde angepasst zu: New Test',
+      absender: 'TestFunk',
+      empfaenger: 'ETB',
+    });
+    expect(prismaService.einsatz.update).toHaveBeenCalledWith({
+      where: { id: einsatzId },
+      data: {
+        einsatz_alarmstichwort: {
+          create: {
+            alarmstichwortId: 'newId',
+          },
+        },
+      },
+    });
+    expect(result).toBeDefined();
+  });
+
+  it('should not change einsatz if alarmstichwort has not changed', async () => {
+    const einsatzId = 'einsatz1';
+    const updateEinsatzDto: UpdateEinsatzDto = { alarmstichwort: 'stichwort1' };
+
+    const result = await service.changeEinsatz(einsatzId, updateEinsatzDto);
+
+    expect(alarmstichwortService.find).not.toHaveBeenCalled();
+    expect(einheitenService.findEinheit).not.toHaveBeenCalled();
+    expect(
+      einsatztagebuchService.createEinsatztagebuchEintrag,
+    ).not.toHaveBeenCalled();
+    expect(prismaService.einsatz.update).not.toHaveBeenCalled();
+    expect(result).toEqual({
+      id: 'einsatz1',
+      aufnehmendesRettungsmittelId: 'rettungsmittel1',
+      einsatz_alarmstichwort: {
+        id: 'stichwort1',
+        bezeichnung: 'Test',
+        beschreibung: 'Test Beschreibung',
+      },
     });
   });
 });
