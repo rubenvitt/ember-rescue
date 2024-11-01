7aaa14cbd920d175c12714ef817d66511cd439e0
Ruben Vitt <dev@rubeen.dev>
2024-11-01 16:13:56 +0100

    feat(database): add mongoose + connection
    ember-rescue-68 Introduce NoSQL

--- backend/src/database/database.providers.ts
+++ backend/src/database/database.providers.ts
@@ -1,4 +1,13 @@
 import { Provider } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import * as mongoose from 'mongoose';
 
-export const databaseProviders: Provider[] = [PrismaService];
+export const databaseProviders: Provider[] = [
+  PrismaService,
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: async (): Promise<typeof mongoose> => {
+      return mongoose.connect(process.env.MONGODB_URL!!);
+    },
+  },
+];
