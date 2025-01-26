505f6ff3379aca174cfc28acccc4df7e7adae5d8
Ruben Vitt <dev@rubeen.dev>
2025-01-26 19:10:27 +0100

    chore(frontend): simplify build process and TypeScript configuration
    - Removed `tsconfig.shared.json` and related build script
    - Disabled strict TypeScript checks for unused locals and parameters
    - Simplified ESLint configuration by commenting out previous setup
    - Added `node_modules` to `.gitignore`

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,22 +1,27 @@
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import js from '@eslint/js';
 
-export default [
-  {
-    ignores: ['**/dist/**', '**/node_modules/**', '**/src-tauri/target/**', '.eslint.config.js'],
-  },
-  {
-    rules: js.configs.all.rules,
-  },
-  {
-    rules: {
-      'max-len': ['warn', { code: 200 }],
-      'no-const-assign': 'warn',
-      'no-undef': 'warn',
-      'no-unused-vars': 'warn',
-      'prefer-const': 'warn',
-      'prettier/prettier': ['error', { endOfLine: 'auto' }],
-    },
-  },
-  eslintPluginPrettierRecommended,
-];
+export default []
+
+// export default [
+//   {
+//     ignores: [
+//       '**/dist/**',
+//       '**/node_modules/**',
+//       '**/src-tauri/target/**',
+//       'eslint.config.js',
+//     ],
+//   },
+//   {
+//     rules: js.configs.all.rules,
+//   },
+//   {
+//     rules: {
+//       'max-len': ['warn', { code: 200 }],
+//       'no-const-assign': 'warn',
+//       'no-undef': 'warn',
+//       'no-unused-vars': 'warn',
+//       'prefer-const': 'warn',
+//       'prettier/prettier': ['error', { endOfLine: 'auto' }],
+//     },
+//   },
+//   eslintPluginPrettierRecommended,
+// ];
