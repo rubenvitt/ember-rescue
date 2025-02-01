d31d987ff44a6fdbb749ea42150d53b92a29fc11
Ruben Vitt <dev@rubeen.dev>
2025-02-01 16:13:49 +0100

    ♻️(ping): Simplify ping controller
    - Remove unnecessary decorators and imports
    - Streamline ping endpoint implementation

--- backend/src/ping/ping.controller.ts
+++ backend/src/ping/ping.controller.ts
@@ -1,11 +1,8 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Get, Res } from '@nestjs/common';
 import { Response } from 'express';
-import { SkipCache } from '@core/interceptors/cache.interceptor';
 
-@Controller('ping')
 export class PingController {
   @Get()
-  @SkipCache()
   ping(@Res() response: Response) {
     response.send({
       message: 'pong',
